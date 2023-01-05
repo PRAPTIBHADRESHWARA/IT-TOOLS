fe7378d6a9ca52f0106e7c7d4fc9d28d57659c71
PRAPTIBHADRESHWARA <121996480+PRAPTIBHADRESHWARA@users.noreply.github.com>
2023-01-05 09:46:59 +0530

    Add files via upload

---
+++ parameter.py
@@ -0,0 +1,8 @@
+def square(x):
+    return 4*x
+def circle(s):
+    return 2*3.14*y
+x = int(input("Enter a number :"))
+print("parameter of square" ,square)
+y = int(input("Enter a number : "))
+print("parameter of circle" ,circle)
